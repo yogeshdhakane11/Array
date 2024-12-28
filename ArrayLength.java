7e148cb5c57a72a87521eba4b03af3f413b7e743
Yogesh <yogeshdhakane2550@gmail.com>
2024-12-28 16:54:39 +0530

    array length

---
+++ ArrayLength.java
@@ -0,0 +1,8 @@
+package Array;
+
+public class ArrayLength {
+    public static void main(String[] args) {
+        int []number = {10, 20, 30, 40, 50, 60, 70, 80, 90};
+        System.out.println("Array Length is : "+ number.length);
+    }
+}
